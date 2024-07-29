2b092bf20001badae8fd0a6d67fd05a5eea30e52
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-07-29 14:51:18 +0300

    new server start

---
+++ js/server.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// const dotenv = require('dotenv');
+// dotenv.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 5151;
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+console.log('sdfsdfefwewefwergergef');
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+// // "exec": "concurrently \"npx tsc --watch\" \"ts-node --files ./server.ts\""
---
+++ server.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import express from 'express';
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5151;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+
+// // "exec": "concurrently \"npx tsc --watch\" \"ts-node --files ./server.ts\""
