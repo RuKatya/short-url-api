a506028700762b438675e96f9418a22506c5b2b2
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-07-29 14:53:08 +0300

    Update server.js

--- js/server.js
+++ js/server.js
@@ -11,7 +11,6 @@ const port = process.env.PORT || 5151;
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
-console.log('sdfsdfefwewefwergergef');
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
